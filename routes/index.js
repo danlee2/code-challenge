a1b20b05ea8e84fe35dfb98c3bd0093899cb9405
danlee2 <danlee1@umbc.edu>
2019-05-28 19:13:56 -0400

    put all static files BACK into the public directory while adjusting pathname in all files

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,19 @@
+const axios = require ("axios");
+axios({
+  url: 'https://next.json-generator.com/api/json/get/EkzBIUWNL',
+  method: 'get'
+})
 
 module.exports = function(app) {
-  // getMany endpoint - using simple GET HTTP request
-    
+  app.get("/", function(req, res, next) {
+    res.render("product");
+  });
+
+
+  // getMany endpoint - using simple GET HTTP request 
+
+
   // getSingle request - using simple GET HTTP request
-};
+
+
+}
--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 'use strict';
 
 // Require necessary npm packages
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 // Setting up port 
-var PORT = process.env.PORT || 8080;
-var jsonURL = "https://next.json-generator.com/api/json/get/EkzBIUWNL";
+let PORT = process.env.PORT || 8080;
 
 // Creating express app by initializing an express server as a variable
-var app = express();
+const app = express();
+var expressLayouts = require('express-ejs-layouts')
 
 app.use(expressLayouts); 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
 app.use(bodyParser.json());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.set('view options', {layout: '/views/layout.ejs'});
 
 // Requiring our routes
 require("./routes/index.js")(app);
+
 
 // this will listen to and show all activities on our terminal to let us know what is happening in our app
 app.listen(PORT, function() {
