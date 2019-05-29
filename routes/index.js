1b9b3b8c88e5a8a067d50c5dbe223aa537e6ce51
danlee2 <danlee1@umbc.edu>
2019-05-28 20:30:24 -0400

    successfully render products on products view from json url

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
 const axios = require ("axios");
-axios({
-  url: 'https://next.json-generator.com/api/json/get/EkzBIUWNL',
-  method: 'get'
-})
 
 module.exports = function(app) {
-  app.get("/", function(req, res, next) {
-    res.render("product");
-  });
-
-
   // getMany endpoint - using simple GET HTTP request 
-
+  app.get('/', function(req, res, next) {
+    axios.get('https://next.json-generator.com/api/json/get/EkzBIUWNL')
+    .then(response => {
+      if (response.data) {
+        res.render('product', {products:response.data});
+      }
+    })
+    .catch(function () { res.render("product", { products: req.flash("error") }); })
+  });
 
   // getSingle request - using simple GET HTTP request
 
