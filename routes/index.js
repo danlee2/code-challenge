55fe7dfccd591b0f4308f234052f9aa6c7184ad7
danlee2 <danlee1@umbc.edu>
2019-05-28 21:46:44 -0400

    successfully render the right item for products-detail view

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,21 @@ module.exports = function(app) {
     .catch(function () { res.render("product", { products: req.flash("error") }); })
   });
 
-  // getSingle request - using simple GET HTTP request
-
-
+  // getSingle request - using simple POST HTTP request
+  app.post('/product-detail',function(req,res,next) {
+    axios.get('https://next.json-generator.com/api/json/get/EkzBIUWNL', {
+      params: {
+        _id: req.body.product_id
+      }
+    })
+    .then(response => {
+      if (response.data) {
+        res.render('product-detail', {detail:response.data});
+      }
+    })
+    .catch(function () { res.render("product-detail", { detail: req.flash("error") }); })
+  });
 }
+
+// weird thing (even in Postman) is that MORE THAN ONE object is returned, although only ONE
+// matches the param specified ... SO DUMB ... hence details[0] on the client side.
